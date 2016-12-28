73693346c08b85d78271e278a6451bcd7b95caae
Sergii Nezdolii <sergii.nezdolii@gmail.com>
2016-12-28 13:10:36 +0100

    Making font map generation more flexible and compatible with swift 3

--- Pod/Assets/generate.rb
+++ Pod/Assets/generate.rb
@@ -1,5 +1,27 @@
 #!/usr/bin/env ruby
 
+class String 
+    def camelize(uppercase_first_letter = true)
+      string = self
+      if uppercase_first_letter
+        string = string.sub(/^[a-z\d]*/) { $&.capitalize }
+      else
+        string = string.sub(/^(?:(?=\b|[A-Z_])|\w)/) { $&.downcase }
+      end
+      string.gsub(/(?:_|(\/))([a-z\d]*)/) { "#{$1}#{$2.capitalize}" }.gsub('/', '::')
+    end
+
+    def undigitalize(digitalize_only_first_symbol = true)
+      string = self
+      if digitalize_only_first_symbol
+        regex = "^[0-9]"
+      else
+        regex = "[0-9]"
+      end
+      string = string.gsub(/#{regex}/, '0' => 'zero_', '1' => 'one_', '2' => 'two_', '3' => 'three_', '4' => 'four_', '5' => 'five_', '6' => 'six_', '7' => 'seven_', '8' => 'eight_', '9' => 'nine_') 
+    end
+end
+
 codepoints = File.open(File.expand_path('../codepoints.txt', __FILE__), 'r')
 outfile = File.expand_path('../../Classes/Font.swift', __FILE__)
 
@@ -7,11 +29,7 @@ names = []
 codes = []
 codepoints.each_line do |line|
   (name, codepoint) = line.split(/ /)
-  if name[0] == '3'
-    # enum cases can't start with a numeral
-    name = 'three_' + name[1..name.size-1]
-  end
-  name = name.split('_').collect(&:capitalize).join
+  name = name.undigitalize.camelize(false)
   names << "case #{name}"
   codes << "\"\\u{#{codepoint.strip}}\""
 end
